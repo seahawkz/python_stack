d0a0f36fc03fb8a4ff58771468dbb5123ba4eaa2
Derek <derekanderson8@msn.com>
2021-04-15 07:51:55 -0700

    first_app

--- django/django_fundamentals/first_django_project/first_django_project/wsgi.py
+++ django/django_fundamentals/first_django_project/first_django_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for your_project_name_here project.
+WSGI config for first_django_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name_here.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first_django_project.settings')
 
 application = get_wsgi_application()
--- django/django_fundamentals/first_django_project/manage.py
+++ django/django_fundamentals/first_django_project/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name_here.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first_django_project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
