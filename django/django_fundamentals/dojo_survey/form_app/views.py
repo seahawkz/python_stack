00b60eae072ba894f2da920e2d021e4929bbe2a7
Derek <derekanderson8@msn.com>
2021-04-22 07:46:24 -0700

    dojo_revision

--- django/django_fundamentals/dojo_survey/form_app/views.py
+++ django/django_fundamentals/dojo_survey/form_app/views.py
@@ -4,14 +4,34 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html')
 
+# def process(request):
+#     if request.method == 'POST':
+#         context = {
+#             'name': request.POST['name'],
+#             'lang': request.POST['language'],
+#             'loc': request.POST['location'],
+#             'flu': request.POST.getlist('fluent[]'),
+#             'com': request.POST['comment']
+#         }
+#         return render(request, 'result.html', context)
+#     return render(request, 'result.html')
+
 def process(request):
-    if request.method == 'POST':
-        context = {
-            'name': request.POST['name'],
-            'lang': request.POST['language'],
-            'loc': request.POST['location'],
-            'flu': request.POST.getlist('fluent[]'),
-            'com': request.POST['comment']
-        }
-        return render(request, 'result.html', context)
-    return render(request, 'result.html')
+    if request.method == 'GET':
+        return redirect('/')
+    request.session['result'] = {
+        'name': request.POST['name'],
+        'lang': request.POST['language'],
+        'loc': request.POST['location'],
+        'flu': request.POST.getlist('fluent[]'),
+        'com': request.POST['comment']
+    }
+    return redirect('/result')
+
+def result(request):
+    context = {
+        'result': request.session['result']
+    }
+    return render(request, 'result.html', context)
+        
+        
