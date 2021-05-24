a41ee3af1b0646262a72ca044313bd706fb21715
Derek <derekanderson8@msn.com>
2021-05-24 13:21:47 -0700

    shows2

--- django/django_orm/tv_shows/add_show/models.py
+++ django/django_orm/tv_shows/add_show/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Show(models.Model):
+    title = models.CharField(max_length=255)
+    network = models.CharField(max_length=45)
+    release_date = models.DateField()
+    description = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{}'.format(self.title)
---
+++ django/django_orm/tv_shows/add_show/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('shows', views.index),
+    path('shows/new', views.new),
+    path('shows/create', views.create),
+    path('shows/<int:show_id>/edit', views.edit),
+    path('shows/<int:show_id>/update', views.update),
+    path('shows/<int:show_id>', views.show),
+    path('shows/<int:show_id>/delete', views.delete),
+]
